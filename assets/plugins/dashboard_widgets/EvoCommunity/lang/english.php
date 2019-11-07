583e102959aa5a06eeecd5b3ba6def93ce4006c2
Andrey <0test@mail.ru>
2019-11-07 13:26:26 +0300

    up

---
+++ assets/plugins/dashboard_widgets/EvoCommunity/lang/english.php
@@ -0,0 +1,14 @@
+<?php 
+$_lang = array();
+
+$_lang['title'] = 'Community';
+$_lang['yt'] = 'Youtube';
+$_lang['telegram1'] = 'Telegram news';
+$_lang['telegram2'] = 'Telegram community';
+$_lang['vk'] = 'VK group';
+$_lang['evoim'] = 'Official site';
+$_lang['modxim'] = 'Community';
+$_lang['docs'] = 'Documents';
+
+
+return $_lang;
---
+++ assets/plugins/dashboard_widgets/EvoCommunity/lang/russian-UTF8.php
@@ -0,0 +1,14 @@
+<?php 
+$_lang = array();
+
+$_lang['title'] = 'Сообщество';
+$_lang['yt'] = 'Youtube';
+$_lang['telegram1'] = 'Новости в Телеграм';
+$_lang['telegram2'] = 'Сообщество в Телеграм';
+$_lang['vk'] = 'Группа ВКонтакте';
+$_lang['evoim'] = 'Официальный сайт';
+$_lang['modxim'] = 'Сайт сообщества';
+$_lang['docs'] = 'Документация';
+
+
+return $_lang;
